6188ad864fa353d9f15c95db3ba5e8a2b40a6c0a
Janez <janez.voler@gmail.com>
2020-08-28 12:28:33 +0200

    dodan django heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import export as export
@@ -148,3 +150,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
